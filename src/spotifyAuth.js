4eb21a6a97ffb744b36d213ff3d6424d090e8bb2
VinnietheGenius <vinniejonesjeyakumar@gmail.com>
2023-05-02 02:40:10 -0400

    more functionality

---
+++ src/spotifyAuth.js
@@ -0,0 +1,18 @@
+const clientId = "something";
+const clientSecret = "something";
+
+const basicAuth = btoa(`${clientId}:${clientSecret}`);
+
+export const getAccessToken = async () => {
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${basicAuth}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: "grant_type=client_credentials",
+  });
+
+  const data = await response.json();
+  return data.access_token;
+};
